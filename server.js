23cb5a50d7785d1f0ec09dc2a8e50e36e3e56190
mayanksharma43 <mayanksharma43@deloitte.com>
2023-07-20 14:06:12 +0530

    tried  connecting to mongodb atlas

--- server.js
+++ server.js
@@ -14,6 +14,47 @@ app.get('/', (req, res) => {
 // Port Number
 const PORT = process.env.PORT ||5000;
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const connectionString = "mongodb+srv://nodejs:helloworld@cluster0.zwfh3di.mongodb.net/?retryWrites=true&w=majority";
+
+async function addData() {
+    try {
+      // Connect to the MongoDB Atlas cluster
+      const client = await MongoClient.connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+  
+      // Access a test collection to insert data
+      const collection = client.db().collection('testCollection');
+  
+      // Data to be inserted into the collection
+      const data = [
+        { name: 'John Doe', age: 30, city: 'New York' },
+        { name: 'Jane Smith', age: 25, city: 'Los Angeles' },
+        { name: 'Bob Johnson', age: 40, city: 'Chicago' },
+      ];
+  
+      // Insert the data into the collection
+      const insertResult = await collection.insertMany(data);
+  
+      console.log('Data inserted successfully:');
+      console.log(insertResult);
+  
+      // Close the connection
+      await client.close();
+    } catch (error) {
+      console.error('Error connecting to MongoDB Atlas:', error);
+    }
+  }
+  
+  addData();
+
+
+
+
 // Server Setup
 app.listen(PORT,console.log(
 `Server started on port ${PORT}`));
